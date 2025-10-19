b81a136598c842c6c21dfb975916b34112aa3a99
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-19 20:10:38 +0000

    Correction for task 3 advanced

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
-"""A script that concatenates two matrices along a specific axis"""
-
-import numpy as np
+"""A script that concatenates two matrices along a specific axis """
 
 
 def cat_matrices(mat1, mat2, axis=0):
     """A function that concatenates two matrices along a specific axis"""
-    try:
-        np_mat1 = np.array(mat1)
-        np_mat2 = np.array(mat2)
+    if axis == 0:
+        if not same_shape(mat1[0], mat2[0]):
+            return None
+        return mat1 + mat2
 
-        result = np.concatenate((np_mat1, np_mat2), axis=axis)
-
-        return result.tolist()
-
-    except Exception:
+    if len(mat1) != len(mat2):
         return None
+
+    new_matrix = []
+    for i in range(len(mat1)):
+        merged = cat_matrices(mat1[i], mat2[i], axis - 1)
+        if merged is None:
+            return None
+        new_matrix.append(merged)
+    return new_matrix
+
+
+def same_shape(a, b):
+    """ Check if two matrices have the same shape (recursively)."""
+    if type(a) is list and type(b) is list:
+        if len(a) != len(b):
+            return False
+        for i in range(len(a)):
+            if not same_shape(a[i], b[i]):
+                return False
+        return True
+    if type(a) is not list and type(b) is not list:
+        return True
+    return False
