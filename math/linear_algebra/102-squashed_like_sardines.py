83d70c262ca66cd71544248d7b512ad8bd9376d0
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-19 20:00:58 +0000

    Update task advanced 3

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
 """A script that concatenates two matrices along a specific axis"""
 
+import numpy as np
+
 
 def cat_matrices(mat1, mat2, axis=0):
     """A function that concatenates two matrices along a specific axis"""
-    if axis == 0:
-        if mat1[0] is list and len(mat1[0]) != len(mat2[0]):
-            return None
-        return mat1 + mat2
+    try:
+        np_mat1 = np.array(mat1)
+        np_mat2 = np.array(mat2)
 
-    if len(mat1) != len(mat2):
+        result = np.concatenate((np_mat1, np_mat2), axis=axis)
+
+        return result.tolist()
+
+    except Exception:
         return None
-
-    result = []
-    for i in range(len(mat1)):
-        sub = cat_matrices(mat1[i], mat2[i], axis - 1)
-        if sub is None:
-            return None
-        result.append(sub)
-
-    return result
