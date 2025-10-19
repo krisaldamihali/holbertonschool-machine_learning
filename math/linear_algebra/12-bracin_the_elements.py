f27ae32f783f4499d9d038bb2c0ff3981fb7a541
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-19 17:57:31 +0000

    Add task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A script that performs element-wise add., sub., multi., and div."""
+
+
+def np_elementwise(mat1, mat2):
+    """ A function that performs element-wise add., sub., multi., and div."""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
