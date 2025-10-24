997db4f2d08f8ee22d1c3ddd2320379488151a63
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-24 13:18:16 +0000

    Add Exponential exercise

---
+++ math/probability/exponential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" A script that represents an exponential distribution"""
+
+
+class Exponential:
+    """A class that represents an exponential distribution:"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Class constructor"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = sum(data) / len(data)
+            self.lambtha = 1 / mean
