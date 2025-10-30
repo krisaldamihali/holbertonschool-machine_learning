41a320f8f6cade71fc97ca48f9b6a437dd99ec60
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-30 20:43:05 +0000

    Exercise 1 - Create DataFrame from a dictionary

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''A script that creates a pd.DataFrame from a dictionary'''
+
+import pandas as pd
+
+data = {
+    'First':  [0.0, 0.5, 1.0, 1.5],
+    'Second': ["one", "two", "three", "four"],
+}
+
+df = pd.DataFrame(data, index=["A", "B", "C", "D"])
