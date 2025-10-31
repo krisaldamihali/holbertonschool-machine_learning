b65a46935f01e5d280abe6bd336c4a089260dfbc
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-31 15:56:31 +0000

    Update exercise 5

--- pipeline/pandas/5-main.py
+++ pipeline/pandas/5-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from_file = __import__('2-from_file').from_file
-slice = __import__('5-slice').slice
+slice_df = __import__('5-slice').slice_df  
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = slice(df)
+df= slice_df(df)
 
 print(df.tail())
--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 '''
-A script that take every 60th row
+A script that takes every 60th row
 of the important columns from a DataFrame
 '''
 
-
-def slice(df):
+def slice_df(df):
     '''
-    A function that take every 60th row
+    A function that takes every 60th row
     of the important columns from a DataFrame
     '''
     df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
