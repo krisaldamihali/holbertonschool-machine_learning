e351146b6bbdba52e2e96ac71c4f79a3e6925354
krisaldamihali <krisaldamihali02@gmail.com>
2025-10-31 16:18:35 +0000

    Change variables names in exercise 5

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -6,10 +6,10 @@ of the important columns from a DataFrame
 
 
 def slice(df):
-    '''
-    A function that take every 60th row
-    of the important columns from a DataFrame
-    '''
-    df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
-    df = df[::60]
-    return df
+    """
+    A function that selects the columns High, Low, Close, and Volume_(BTC),
+    then takes every 60th row from the DataFrame.
+    """
+    selected_columns = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
+    sliced_df = selected_columns[::60]
+    return sliced_df
