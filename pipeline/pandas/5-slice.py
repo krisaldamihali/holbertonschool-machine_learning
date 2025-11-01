d5d59d5179663b4f6c413e20e0dce2079366bc9c
krisaldamihali <krisaldamihali02@gmail.com>
2025-11-01 16:19:15 +0000

    Task 5 updated comments

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-"""
-A script that take every 60th row of the important columns from a DataFrame
-"""
+""" A script that slices a pandas DataFrame"""
 
 
 def slice(df):
-    """
-    A function that selects the columns High, Low, Close, and Volume_(BTC),
-    then takes every 60th row from the DataFrame.
-    """
+    """A function that returns a sliced DataFrame"""
     selected_columns = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
     sliced_df = selected_columns[::60]
     return sliced_df
