a9e783e1c3d550fea9590f11eeb748ce4d16c768
krisaldamihali <krisaldamihali02@gmail.com>
2025-11-04 22:46:44 +0000

    Add advanced task 2

---
+++ math/plotting/101-pca.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+lib = np.load("pca.npz")
+data = lib["data"]
+labels = lib["labels"]
+
+data_means = np.mean(data, axis=0)
+norm_data = data - data_means
+_, _, Vh = np.linalg.svd(norm_data)
+pca_data = np.matmul(norm_data, Vh[:3].T)
+
+# your code here
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d")
+
+sc = ax.scatter(
+    pca_data[:, 0],
+    pca_data[:, 1],
+    pca_data[:, 2],
+    c=labels,
+    cmap="plasma",
+    s=50,
+    edgecolor="k"
+)
+
+ax.set_xlabel("U1", fontsize=10)
+ax.set_ylabel("U2", fontsize=10)
+ax.set_zlabel("U3", fontsize=10)
+ax.set_title("PCA of Iris Dataset", fontsize=12, pad=10)
+
+plt.show()
