0199a90e7f263f811d83248d16a66057c9d8888b
krisaldamihali <krisaldamihali02@gmail.com>
2025-11-14 19:44:16 +0000

    Add the number of launches per rocket

---
+++ pipeline/apis/4-rocket_frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+A script that displays the number of launches per rocket
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.spacexdata.com/v4/launches'
+    results = requests.get(url).json()
+    rocketDict = {}
+    for launch in results:
+        rocket = launch.get('rocket')
+        url = 'https://api.spacexdata.com/v4/rockets/{}'.format(rocket)
+        results = requests.get(url).json()
+        rocket = results.get('name')
+        if rocketDict.get(rocket) is None:
+            rocketDict[rocket] = 1
+        else:
+            rocketDict[rocket] += 1
+
+    rocketList = sorted(rocketDict.items(), key=lambda kv: kv[0])
+    rocketList = sorted(rocketList, key=lambda kv: kv[1], reverse=True)
+
+    for rocket in rocketList:
+        print("{}: {}".format(rocket[0], rocket[1]))
