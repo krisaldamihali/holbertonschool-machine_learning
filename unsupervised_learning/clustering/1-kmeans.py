bb359d828778ab5a00e4b51b3f45a71692461f26
krisaldamihali <krisaldamihali02@gmail.com>
2025-12-23 09:08:10 +0000

    update

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -29,22 +29,32 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None
 
-    if not isinstance(k, int) or k <= 0:
+    if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
         return None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    C = initialize(X, k)
+    n, d = X.shape
+
+    min_values = np.min(X, axis=0)
+    max_values = np.max(X, axis=0)
+    C = np.random.uniform(low=min_values, high=max_values, size=(k, d))
 
     for _ in range(iterations):
         previous_C = C.copy()
+
         distances = np.linalg.norm(X[:, None, :] - C, axis=2)
         clss = np.argmin(distances, axis=1)
+
         for i in range(k):
             points_in_cluster = X[clss == i]
             if points_in_cluster.shape[0] == 0:
-                C[i] = initialize(X, 1)[0]
+                C[i] = np.random.uniform(
+                    low=min_values,
+                    high=max_values,
+                    size=d
+                )
             else:
                 C[i] = np.mean(points_in_cluster, axis=0)
 
