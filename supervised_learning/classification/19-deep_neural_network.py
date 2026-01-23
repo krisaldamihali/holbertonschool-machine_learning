79a6edacc7a3a0dec0e7a0782869a5ab8e0c6b1e
krisaldamihali <krisaldamihali02@gmail.com>
2026-01-23 21:56:20 +0100

    Task 19: Deep_neural_network

---
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+A script that implements a deep neural network for binary classification.
+"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    A class that implements a deep neural network for binary classification.
+    """
+
+    def __init__(self, nx, layers):
+        """
+        A constructor that takes number of input as nx and
+        layers is a list representing the number of nodes
+        in each layer of the network
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        prev = nx
+        for layer in range(1, self.L + 1):
+            nodes = layers[layer - 1]
+
+            if type(nodes) is not int or nodes <= 0:
+                raise TypeError("layers must be a list of positive integers")
+
+            self.weights["W{}".format(layer)] = (
+                np.random.randn(nodes, prev) * np.sqrt(2 / prev)
+            )
+            self.weights["b{}".format(layer)] = np.zeros((nodes, 1))
+            prev = nodes
+
+    @property
+    def L(self):
+        """
+        A function that gets the number of layers in the neural network.
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """
+        A function that gets a dictionary holding all
+        intermediary values of the network.
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """
+        A function that gets a dictionary holding
+        all weights and biases of the network.
+        """
+        return self.__weights
+
+    def forward_prop(self, X):
+        """
+        A function that calculates the forward propagation
+        of the neural network
+        """
+        self.__cache["A0"] = X
+
+        for layer in range(1, self.__L + 1):
+            W = self.__weights[f"W{layer}"]
+            b = self.__weights[f"b{layer}"]
+
+            A_prev = self.__cache[f"A{layer-1}"]
+
+            Z = np.matmul(W, A_prev) + b
+            A = 1 / (1 + np.exp(-Z))
+
+            self.__cache[f"A{layer}"] = A
+
+        return self.__cache[f"A{self.__L}"], self.__cache
+
+    def cost(self, Y, A):
+        """
+        A function that calculates the cost of the model
+        using logistic regression
+        """
+        m = Y.shape[1]
+
+        log_loss = -1/m*np.sum(Y * np.log(A) + (1-Y)*(np.log(1.0000001-A)))
+
+        return log_loss
