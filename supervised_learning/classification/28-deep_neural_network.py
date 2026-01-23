a6009a6759f264a3bd531f69d3c4df6ed64421b2
krisaldamihali <krisaldamihali02@gmail.com>
2026-01-23 22:54:34 +0100

    Update tasks: Deep_neural_network

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -32,18 +32,19 @@ class DeepNeuralNetwork:
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
+        self.__activation = activation
 
         prev = nx
-        for layer in range(1, self.L + 1):
+        for layer in range(1, self.__L + 1):
             nodes = layers[layer - 1]
 
             if type(nodes) is not int or nodes <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
-            self.weights["W{}".format(layer)] = (
+            self.__weights["W{}".format(layer)] = (
                 np.random.randn(nodes, prev) * np.sqrt(2 / prev)
             )
-            self.weights["b{}".format(layer)] = np.zeros((nodes, 1))
+            self.__weights["b{}".format(layer)] = np.zeros((nodes, 1))
             prev = nodes
 
     @property
@@ -68,6 +69,13 @@ class DeepNeuralNetwork:
         all weights and biases of the network.
         """
         return self.__weights
+    
+    @property
+    def activation(self):
+        """
+        A function that returns the activation function used by the network.
+        """
+        return self.__activation
 
     def forward_prop(self, X):
         """
@@ -77,17 +85,24 @@ class DeepNeuralNetwork:
         self.__cache["A0"] = X
 
         for layer in range(1, self.__L + 1):
-            W = self.__weights[f"W{layer}"]
-            b = self.__weights[f"b{layer}"]
-
-            A_prev = self.__cache[f"A{layer-1}"]
+            W = self.__weights["W{}".format(layer)]
+            b = self.__weights["b{}".format(layer)]
+            A_prev = self.__cache["A{}".format(layer - 1)]
 
             Z = np.matmul(W, A_prev) + b
-            A = 1 / (1 + np.exp(-Z))
 
-            self.__cache[f"A{layer}"] = A
+            if layer != self.__L:
+                if self.__activation == 'sig':
+                    A = 1 / (1 + np.exp(-Z))
+                else:
+                    A = np.tanh(Z)
+            else:
+                Z_exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+                A = Z_exp / np.sum(Z_exp, axis=0, keepdims=True)
 
-        return self.__cache[f"A{self.__L}"], self.__cache
+            self.__cache["A{}".format(layer)] = A
+
+        return self.__cache["A{}".format(self.__L)], self.__cache
 
     def cost(self, Y, A):
         """
@@ -96,21 +111,21 @@ class DeepNeuralNetwork:
         """
         m = Y.shape[1]
 
-        log_loss = -1/m*np.sum(Y * np.log(A) + (1-Y)*(np.log(1.0000001-A)))
+        cost = (-1 / m) * np.sum(Y * np.log(A))
 
-        return log_loss
+        return cost
 
     def evaluate(self, X, Y):
         """
         A function that evaluates the neural network's predictions
         """
         A, _ = self.forward_prop(X)
+        cost = self.cost(Y, A)
 
-        prediction = (A >= 0.5).astype(int)
+        predictions = np.zeros_like(A)
+        predictions[np.argmax(A, axis=0), np.arange(A.shape[1])] = 1
 
-        cost_value = self.cost(Y, A)
-
-        return prediction, cost_value
+        return predictions, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """
@@ -122,44 +137,27 @@ class DeepNeuralNetwork:
         dZ = None
 
         for layer in range(self.__L, 0, -1):
-            A = cache[f"A{layer}"]
-            A_prev = cache[f"A{layer-1}"]
+            A = cache["A{}".format(layer)]
+            A_prev = cache["A{}".format(layer - 1)]
 
             if layer == self.__L:
                 dZ = A - Y
             else:
-                W_next = weights_copy[f"W{layer + 1}"]
-                dZ = np.matmul(W_next.T, dZ) * (A * (1 - A))
+                W_next = weights_copy["W{}".format(layer + 1)]
+                if self.__activation == 'sig':
+                    dZ = np.matmul(W_next.T, dZ) * (A * (1 - A))
+                else:
+                    dZ = np.matmul(W_next.T, dZ) * (1 - A ** 2)
 
             dW = (1/m) * np.matmul(dZ, A_prev.T)
             db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
 
-            self.__weights[f"W{layer}"] = (
-                weights_copy[f"W{layer}"] - alpha * dW
+            self.__weights["W{}".format(layer)] = (
+                weights_copy["W{}".format(layer)] - alpha * dW
             )
-            self.__weights[f"b{layer}"] = (
-                weights_copy[f"b{layer}"] - alpha * db
+            self.__weights["b{}".format(layer)] = (
+                weights_copy["b{}".format(layer)] - alpha * db
             )
-
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """
-        A function that trains the deep neural network
-        """
-        if type(iterations) is not int:
-            raise TypeError("iterations must be an integer")
-        if iterations <= 0:
-            raise ValueError("iterations must be a positive integer")
-
-        if type(alpha) is not float:
-            raise TypeError("alpha must be a float")
-        if alpha <= 0:
-            raise ValueError("alpha must be positive")
-
-        for i in range(iterations):
-            A, cache = self.forward_prop(X)
-            self.gradient_descent(Y, cache, alpha)
-
-        return self.evaluate(X, Y)
 
     def train(self, X, Y, iterations=5000, alpha=0.05,
               verbose=True, graph=True, step=100):
@@ -191,7 +189,7 @@ class DeepNeuralNetwork:
 
             if i == 0 or i == iterations or i % step == 0:
                 if verbose:
-                    print(f"Cost after {i} iterations: {cost}")
+                    print("Cost after {} iterations: {}".format(i, cost))
                 if graph:
                     costs.append(cost)
                     iteration_list.append(i)
