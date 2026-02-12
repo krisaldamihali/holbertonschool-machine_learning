d2a1c006a0a04a6a610ea601decc837f2aaebe8a
krisaldamihali <krisaldamihali02@gmail.com>
2026-02-12 23:59:22 +0100

    Update tasks

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,15 +7,6 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     A function that creates a neural network layer with L2 regularization.
-
-    Args:
-        prev: tensor containing the output of the previous layer
-        n: number of nodes the new layer should contain
-        activation: activation function for the layer
-        lambtha: L2 regularization parameter
-
-    Returns:
-        Output of the new layer
     """
     initializer = tf.keras.initializers.VarianceScaling(
         scale=2.0,
